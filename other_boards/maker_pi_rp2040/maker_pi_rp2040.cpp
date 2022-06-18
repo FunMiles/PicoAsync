5590ea0c650bb6cf2b8f8b4d35797184726d5ce1
Michel Lesoinne <michel.lesoinne@androdevllc.com>
2022-06-17 21:23:34 -0600

    feat: Blink blue LEDs in sequence.

--- other_boards/maker_pi_rp2040/maker_pi_rp2040.cpp
+++ other_boards/maker_pi_rp2040/maker_pi_rp2040.cpp
@@ -7,22 +7,32 @@
 
 using namespace std::chrono_literals;
 using events::operator co_await;
-const uint led_pin = 6;
+const uint    led_pin = 6;
 
-/// Task blinking the LED twice per second.
-task<> blink(uint pin, uint delay) {
-	gpio_init(pin);
-	gpio_set_dir(pin, GPIO_OUT);
+const uint led_pins[] = {0, 1, 2, 3, 4, 5, 6, 7, 16, 17, 26, 27, 28};
 
-	for (int i = 0; true; ++i) {
-		co_await (delay * 1ms);
-		gpio_put(pin, 1);
-		co_await (delay * 1ms);
-		gpio_put(pin, 0);
+/// Task blinking All blue LEDs in sequence.
+template <size_t N>
+task<>
+blink(const uint (&pins)[N], uint delay)
+{
+	for (auto pin : pins) {
+		gpio_init(pin);
+		gpio_set_dir(pin, GPIO_OUT);
+	}
+
+	while (true) {
+		for (auto pin : pins) {
+			gpio_put(pin, 1);
+			co_await (delay * 1ms);
+			gpio_put(pin, 0);
+		}
 	}
 }
 
-int main() {
+int
+main()
+{
 	// Start the main loop with two tasks.
-  loop_control.loop(blink(led_pin, 250));
+	loop_control.loop(blink(led_pins, 250));
 }
