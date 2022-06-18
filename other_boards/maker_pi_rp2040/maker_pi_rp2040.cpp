8b406584e663801163b72f52cb95271aad535f11
Michel Lesoinne <michel.lesoinne@androdevllc.com>
2022-06-18 12:02:54 -0600

    feat: Add NeoPixel color changes to the maker_pi_rp2040 demo.

--- other_boards/maker_pi_rp2040/maker_pi_rp2040.cpp
+++ other_boards/maker_pi_rp2040/maker_pi_rp2040.cpp
@@ -3,7 +3,11 @@
 //
 #include "Async/events.h"
 #include "Async/task.h"
+
+#include "devices/neopixel/NeoPixel.h"
 #include "pico/stdlib.h"
+
+#include "hardware/pio.h"
 
 using namespace std::chrono_literals;
 using events::operator co_await;
@@ -30,9 +34,50 @@ blink(const uint (&pins)[N], uint delay)
 	}
 }
 
+task<>
+neoPixel(uint pin)
+{
+	const int numLEDs = 2;
+	const int stateMachine = 0;
+	NeoPixel np(pin, numLEDs, pio0, stateMachine);
+	int color = 1;
+	for (int i = 0; i < 10; ++i) {
+		co_await 1s;
+		std::cout << "Wait for it! " << i << std::endl;
+	}
+	while(true) {
+		co_await 1s;
+		np.clear();
+		std::cout << "Color " << color << std::endl;
+		switch (color % 3) {
+		case 0:
+			np.push_back({128,0,0});
+			np.push_back({0,128,0});
+			break;
+		case 1:
+			np.push_back({0,128,0});
+			np.push_back({0,0,128});
+			break;
+		case 2:
+			np.push_back({0,0,128});
+			np.push_back({128,0,0});
+			break;
+		default:
+			np.push_back({128,0,0});
+			np.push_back({0,128,0});
+			break;
+		}
+		std::cout << "Ready to show " << np.size() << " pixels." << std::endl;
+		co_await np.show();
+		++color;
+		std::cout <<"New color is " << color << std::endl;
+	}
+}
+
 int
 main()
 {
+	stdio_init_all();
 	// Start the main loop with two tasks.
-	loop_control.loop(blink(led_pins, 250));
+	loop_control.loop(blink(led_pins, 250), neoPixel(18));
 }
